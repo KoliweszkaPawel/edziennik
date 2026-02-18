110c7bb2d1ecf6f22e154cfdd4ab7d572935a69c
Paweł Koliweszka <pawelkoliweszka@gmail.com>
2026-02-18 22:09:08 +0100

    Połączenie firebase za pomocą biblioteki angular fire

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyApOfRhZLuFpjzt9LgWCV1b7T7KNbH0cos",
+    authDomain: "edziennik-d77e2.firebaseapp.com",
+    projectId: "edziennik-d77e2",
+    storageBucket: "edziennik-d77e2.firebasestorage.app",
+    messagingSenderId: "28820903036",
+    appId: "1:28820903036:web:b4970b6bbb06d5054665f9",
+    measurementId: "G-N1CM0BMKN1"
+  }
+};
