7f2bd635b1230e95d8dda4cc8e34b0c6ccfb539a
Paweł Koliweszka <pawelkoliweszka@gmail.com>
2026-02-22 15:23:14 +0100

    Rozdzielenie zawrtości dashboardu ze względu na rolę użytkownika na dwa osobne komponenty

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,70 +1,24 @@
 import {Component, inject} from '@angular/core';
 import {AuthorizationService} from '../../core/auth/services/authorization.service';
-import {GradeService} from '../../features/grades/services/grade.service';
-import {Grade} from '../../features/grades/models/grade.model';
-import {MatButton} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
 import {toSignal} from '@angular/core/rxjs-interop';
-import {DatePipe} from '@angular/common';
-import {FormControl, FormGroup, FormGroupDirective, ReactiveFormsModule, Validators} from '@angular/forms';
-import {StudentService} from '../../features/users/services/student.service';
-import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
-import {MatOption, MatSelect} from '@angular/material/select';
+import {ReactiveFormsModule} from '@angular/forms';
+import {GradesTable} from '../grades-table/grades-table';
+import {NewGradeForm} from '../new-grade-form/new-grade-form';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
-    MatButton,
     MatTableModule,
-    DatePipe,
     ReactiveFormsModule,
-    MatFormField,
-    MatLabel,
-    MatInput,
-    MatSelect,
-    MatOption
+    GradesTable,
+    NewGradeForm
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
   private authorization = inject(AuthorizationService);
-  private gradeService = inject(GradeService);
-  private studentService = inject(StudentService);
-  private uid = this.authorization.currentUser()!.uid;
+  protected uid = this.authorization.currentUser()!.uid;
   protected userProfile = toSignal(this.authorization.getUserProfile(this.uid));
-  protected grades = toSignal(this.gradeService.getGrades(this.uid), { initialValue: []});
-  protected students = toSignal(this.studentService.getStudents(), { initialValue: []});
-  displayedColumns = ['subject', 'value', 'weight', 'createdAt', 'teacher'];
-
-  newGradeForm = new FormGroup({
-    studentId: new FormControl('', {nonNullable: true, validators: Validators.required}),
-    subject: new FormControl('', {nonNullable: true, validators: Validators.required}),
-    value: new FormControl<number>(0, {nonNullable: true, validators: Validators.required}),
-    weight: new FormControl<number>(0, {nonNullable: true, validators: Validators.required}),
-    description: new FormControl('', {nonNullable: true, validators: Validators.required}),
-  });
-
-  onSubmit(formDir: FormGroupDirective) {
-    if(this.newGradeForm.valid) {
-      const formValue = this.newGradeForm.getRawValue();
-      const newGrade: Grade = {
-        studentId: formValue.studentId,
-        subject: formValue.subject,
-        value: formValue.value,
-        weight: formValue.weight,
-        description: formValue.description,
-        createdAt: new Date,
-        teacher: this.userProfile()?.email!
-      }
-      this.gradeService.addGrade(newGrade).subscribe({
-        next: () => {
-          formDir.resetForm();
-        },
-        error: (err) => {
-          console.error('Błąd podczas zapisywania oceny', err);
-        }
-      });
-    }
-  }
 }
